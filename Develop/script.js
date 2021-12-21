4ac4d1bc54b409636dba099188a71cdd3cfb59c0
samuel lutz <samlutz89@gmail.com>
2021-12-20 16:34:35 -0800

    adding code to js

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,8 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+function writePassword()
+enter = parseInt(prompt("how many characters would you like your password to be? choose between 8-128"))
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
