93154482f121845563d5c87588e582ce8f096bde
samuel lutz <samlutz89@gmail.com>
2021-12-22 09:08:38 -0800

    begining to add random letters and numbers

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowerCase = ("abcdefghijklmnopqrstuvwxyz") .split();
+var upperCase = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ") .split();
+var numbers = ("0123456789") .split();
+var specialChar = ("!@#$%^&*") .split();
 
 // Write password to the #password input
-function writePassword()
-enter = parseInt(prompt("how many characters would you like your password to be? choose between 8-128"))
+function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -11,5 +14,37 @@ enter = parseInt(prompt("how many characters would you like your password to be?
 
 }
 
+function generatePassword() {
+ console.log ("button click")
+  var passLength = parseInt(prompt("please select character length between 8 and 128."));
+
+  var lengthConfirm = parseInt(passLength);
+  console.log(lengthConfirm);
+
+  if (passLength < 8 || passLength > 128 || isNaN(passLength)) {
+    alert ("error, choice is not valid. please try again.");
+    return;
+  }
+
+  var lowerCaseConfirm = confirm("Do you want your password to include lower case letters?");
+    if(lowerCaseConfirm === true){
+      for (i = 0; i < lowerCase.length; i++)
+    }
+
+   var upperCaseConfirm = confirm("Do you want your password to include upper case letters?");
+    if(upperCaseConfirm === true){
+      for (i = 0; i < upperCase.length; i++)
+    }
+
+    var numberConfirm = confirm ("do you want to include numbers?");
+      if (numberConfirm === true){
+        for (i = 0; i < numbers.length; i++)
+      }
+
+    var SpecialCharacter = confirm("do you want to include special characters?")
+      if (SpecialCharacter === true){
+        for (i = 0; i < specialChar.length; i++)
+      }
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
