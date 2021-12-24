3429bfad2d1efd2c8d1a9763984062a05ebbcc50
samuel lutz <samlutz89@gmail.com>
2021-12-24 14:50:39 -0800

    starting over

--- Develop/script.js
+++ Develop/script.js
@@ -1,69 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowerCase = ("abcdefghijklmnopqrstuvwxyz").split("");
-var upperCase = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ").split("");
-var numbers = ("0123456789") .split("");
-var specialChar = ("!@#$%^&*") .split("");
+var confirmNumber;
+var confirmCharacter;
+var confirmUppercase;
+var confirmLowercase;
+var choices;
 
+// added all special characters, letters, and numbers
+
+lowerCase = ("abcdefghijklmnopqrstuvwxyz").split("");
+upperCase = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ").split("");
+numbers = ("0123456789") .split("");
+specialChar = ("!@#$%^&*") .split("");
+var get = document.querySelector("#generate");
+
+
+get.addEventListener("click", function () {
+    ps = generatePassword();
+    document.getElementById("password").placeholder = ps;
+});
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  console.log(password)
-
-  passwordText.value = password;
-
-}
-
 function generatePassword() {
-  var  passwordtotal = "";
-  var result = "";
- console.log ("button click")
-  var passLength = parseInt(prompt("please select character length between 8 and 128."));
-console.log(passLength)
-
-  var lengthConfirm = parseInt(passLength);
-  console.log(lengthConfirm);
-
-  if (passLength < 8 || passLength > 128 || isNaN(passLength)) {
-    alert ("error, choice is not valid. please try again.");
-    return;
-  }
-
-  var lowerCaseConfirm = confirm("Do you want your password to include lower case letters?");
-  if(lowerCaseConfirm === true){
-    for (var i = 0; i < lowerCase.length; i++) {
-      passwordtotal.push(specialChar[i]);
-   }
-  }
-   var upperCaseConfirm = confirm("Do you want your password to include upper case letters?");
-   if(upperCaseConfirm === true){
-    for (var i = 0; i < upperCase.length; i++) {
-      passwordtotal.push(specialChar[i]);
-    }
-    }
-    var numberConfirm = confirm ("do you want to include numbers?");
-    if(numberConfirm === true){
-     for (var i = 0; i < numbers.length; i++) {
-      passwordtotal.push(specialChar[i]);
-    }
-   }
-    var SpecialCharacter = confirm("do you want to include special characters?")
-    if(SpecialCharacter === true){
-      for (var i = 0; i < specialChar.length; i++) {
-        passwordtotal.push(specialChar[i]);
-    }
-   }
-  for (i = 0; i < lengthConfirm; i++) {
-    passwordtotal[
-      Math.floor(Math.random() * passLength.length)];
-   result +=
-      passwordtotal[
-        Math.floor(Math.random() * passLength.length)
-      ];
-    }
-  console.log(i)
-   return result;
-  }
-// Ad event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  // Asks for user input
+  
